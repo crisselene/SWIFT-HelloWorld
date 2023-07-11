a417b5341393db3a47f3dea3b3179d4f5473fb77
crisselene <criisalways@gmail.com>
2023-07-11 09:36:03 +0200

    primera prueba carpetas

---
+++ Swift-hello-world-1/Soporte-GitHub-HelloWorld-crisseleneUITests/Soporte_GitHub_HelloWorld_crisseleneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Soporte_GitHub_HelloWorld_crisseleneUITestsLaunchTests.swift
+//  Soporte-GitHub-HelloWorld-crisseleneUITests
+//
+//  Created by Díez Sobrino Cristina on 10/7/23.
+//
+
+import XCTest
+
+final class Soporte_GitHub_HelloWorld_crisseleneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
